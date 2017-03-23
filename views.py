badb565b37c6b2a2850eba67592f7131a894d406
Shashwat Vikram <shashwat.vikram50@gmail.com>
2017-03-23 22:38:28 +0530

    Rendering basic webpage using template inheritence

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def homepage():
-    return "The only way in is out !"
+    return render_template("pretty.html")
+
+@app.route("/basic")
+def basic():
+    return render_template("basic.html")
